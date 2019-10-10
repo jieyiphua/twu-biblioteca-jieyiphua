a3728618935f6693f2ff3282389903e175ead713
Jie Yi Phua <jieyiphua@Jies-MBP.gateway>
2019-10-11 01:11:59 +1100

    Added exit option in menu and menu is looped until user decides to exit

--- src/com/twu/biblioteca/MenuOptions.java
+++ src/com/twu/biblioteca/MenuOptions.java
@@ -6,25 +6,44 @@ public class MenuOptions {
         System.out.println("Menu");
         System.out.println("__________________________");
         System.out.println("Press 1 for List of Books");
+        System.out.println("Press 0 to Quit");
+    }
+
+    public static void menuOptionList(String userInput) {
+        switch (userInput){
+            case "1":
+                Book book1 = new Book("Harry Potter", "JK Rowling", 1000);
+                Book book2 = new Book("Lord Of The Ring", "J.R.R Tolkien", 2000);
+                BookList.addBook(book1);
+                BookList.addBook(book2);
+                BookList.getBookList();
+                break;
+
+            case "0":
+                MenuOptions.exitMessage();
+                break;
+
+            default:
+                MenuOptions.errMessage();
+        }
     }
 
     public static void menuLoop (String userInput) {
 
-        if (userInput.equals("1")) {
-            Book book1 = new Book("Harry Potter", "JK Rowling", 1000);
-            Book book2 = new Book("Lord Of The Ring", "J.R.R Tolkien", 2000);
-            BookList.addBook(book1);
-            BookList.addBook(book2);
-            BookList.getBookList();
-        }
-        else{
-            MenuOptions.errMessage();
+        MenuOptions.menuOptionList(userInput);
+
+        if (!userInput.equals("0")) {
+            MenuOptions.showMenu();
+            userInput = BibliotecaApp.getUserInput();
+            MenuOptions.menuLoop(userInput);
         }
     }
 
-        public static void errMessage() {
-            System.out.println("Please select a valid option!");
+    public static void errMessage() {
+        System.out.println("Please select a valid option!");
+    }
 
-        }
-
+    public static void exitMessage() {
+        System.out.println("Thank you for using Biblioteca!");
+    }
 }
--- test/com/twu/biblioteca/MenuOptionsTest.java
+++ test/com/twu/biblioteca/MenuOptionsTest.java
@@ -1,12 +1,8 @@
 package com.twu.biblioteca;
 
 import org.junit.Test;
-
-
-import java.awt.*;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
 import static org.junit.Assert.*;
 
 public class MenuOptionsTest {
@@ -15,7 +11,7 @@ public class MenuOptionsTest {
        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
        System.setOut(new PrintStream(outContent));
        MenuOptions.showMenu();
-       assertEquals("Menu" + "\n" + "__________________________" + "\n" + "Press 1 for List of Books" + "\n",outContent.toString());
+       assertEquals("Menu" + "\n" + "__________________________" + "\n" + "Press 1 for List of Books" + "\n" + "Press 0 to Quit" + "\n", outContent.toString());
    }
 
    @Test
@@ -23,20 +19,36 @@ public class MenuOptionsTest {
       final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
       System.setOut(new PrintStream(outContent));
       MenuOptions.errMessage();
-      assertEquals("Please select a valid option!\n",outContent.toString());
+      assertEquals("Please select a valid option!\n", outContent.toString());
    }
 
    @Test
-   public void validOptionAccepted() {
-      MenuOptions.menuLoop("1");
+   public void exitMessage() {
+      final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+      System.setOut(new PrintStream(outContent));
+      MenuOptions.exitMessage();
+      assertEquals("Thank you for using Biblioteca!\n", outContent.toString());
+   }
+
+   @Test
+   public void validOptionOne() {
+      MenuOptions.menuOptionList("1");
       assertEquals("Harry Potter", BookList.bookList.get(0).bookName);
+   }
+
+   @Test
+   public void validOptionZero() {
+      final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+      System.setOut(new PrintStream(outContent));
+      MenuOptions.menuOptionList("0");
+      assertEquals("Thank you for using Biblioteca!\n", outContent.toString());
    }
 
    @Test
    public void nonValidOptionRejected() {
       final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
       System.setOut(new PrintStream(outContent));
-      MenuOptions.menuLoop("pop");
+      MenuOptions.menuOptionList("pop");
       assertEquals("Please select a valid option!\n", outContent.toString());
    }
 }
