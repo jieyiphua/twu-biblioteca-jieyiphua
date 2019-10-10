dba965a2675283dffa5fda99632816a5a7327d5b
Jie Yi Phua <jieyiphua@Jies-MBP.gateway>
2019-10-10 17:39:26 +1100

    Added Book class & tests

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,4 +1,25 @@
 package com.twu.biblioteca;
 
 public class Book {
+    public String bookName;
+    public String author;
+    public int yearPublished;
+
+
+    public Book(String bookName, String author, int yearPublished) {
+        this.bookName = bookName;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    public void showBook() {
+        System.out.println(
+                "Book Name: " +
+                this.bookName +
+                "\nAuthor: " +
+                this.author +
+                "\nYear Published: " +
+                this.yearPublished );
+    }
+
 }
---
+++ test/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,21 @@
+package com.twu.biblioteca;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class BookTest {
+
+    @Test
+    public void showsBookInformation() {
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        Book book1 = new Book("BookName","Author",1000);
+        book1.showBook();
+        assertEquals("Book Name: " + book1.bookName + "\nAuthor: " + book1.author +"\nYear Published: " + book1.yearPublished + "\n",outContent.toString());
+    }
+
+
+}
