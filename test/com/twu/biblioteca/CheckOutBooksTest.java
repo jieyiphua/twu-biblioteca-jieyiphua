f44a7546c5213b6f40ecaf2c143082fb5ae5f2d4
Jie Yi Phua <jieyiphua@Jies-MacBook-Pro.local>
2019-10-14 16:43:07 +1100

    Refactored Check Out Books Test

--- test/com/twu/biblioteca/CheckOutBooksTest.java
+++ test/com/twu/biblioteca/CheckOutBooksTest.java
@@ -1,37 +1,80 @@
 package com.twu.biblioteca;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import static org.hamcrest.CoreMatchers.containsString;
 
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.*;
 
 public class CheckOutBooksTest {
-    @Test
-    public void canCheckOut() {
-        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        Book bookInLibrary = new Book("Library","Author",2000);
-        CheckOutBooks.canCheckOut(bookInLibrary);
-        assertEquals(bookInLibrary.checkedOut,true);
+
+    private static ByteArrayOutputStream byteArrayOutputStream;
+    private static Book bookSample;
+    private static Book bookSample2;
+
+    @Before
+    public void setUp() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        bookSample = new Book("BookName","Author",1000);
+        bookSample2 = new Book("BookName2","Author2",2000);
+    }
+
+    @After
+    public void clearBookList() {
+        BookList.bookList.clear();
     }
 
     @Test
-    public void cannotCheckOut() {
-        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        Book checkedOutBook = new Book("Harry Potter", "JK Rowling", 1000);
-        checkedOutBook.checkedOut = true;
-        CheckOutBooks.canCheckOut(checkedOutBook);
-        assertEquals("Sorry, that book is not available\n",outContent.toString());
+    public void testCanCheckOut() {
+        CheckOutBooks.canCheckOut(bookSample);
+        assertEquals(bookSample.checkedOut,true);
     }
 
     @Test
-    public void currentCheckOutBook() {
-        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        Book checkedOutBook = new Book("Harry Potter", "JK Rowling", 1000);
-        CheckOutBooks.currentCheckOutBook(checkedOutBook);
-        assertEquals("Thank you! Enjoy the book\n" + "Book Name: " + "Harry Potter" + "\nAuthor: " + "JK Rowling" +"\nYear Published: " + 1000 + "\n\n", outContent.toString());
+    public void testCannotCheckOut() {
+        bookSample2.checkedOut = true;
+        CheckOutBooks.canCheckOut(bookSample2);
+        assertEquals("Sorry, that book is not available\n", byteArrayOutputStream.toString());
+    }
+
+    @Test
+    public void testCheckIfNonIntegerInputIsAppropriate() {
+        CheckOutBooks.checkIfInputIsAppropriate("Hello");
+        assertEquals("Sorry, that book is not available\n", byteArrayOutputStream.toString());
+    }
+
+    @Test
+    public void testCheckIfInvalidInputIsAppropriate() {
+        BookList.addBook(bookSample);
+        CheckOutBooks.checkIfInputIsAppropriate("2");
+        assertEquals("Sorry, that book is not available\n", byteArrayOutputStream.toString());
+    }
+
+    @Test
+    public void testCheckIfValidInputIsAppropriate() {
+        BookList.addBook(bookSample);
+        CheckOutBooks.checkIfInputIsAppropriate("1");
+        assertEquals(bookSample.checkedOut, true);
+    }
+
+    @Test
+    public void testCurrentCheckOutBook() {
+        CheckOutBooks.currentCheckOutBook(bookSample);
+        assertThat(byteArrayOutputStream.toString(), containsString("Thank you! Enjoy the book"));
+    }
+
+    @Test
+    public void testShowBookListSelection() {
+        BookList.addBook(bookSample);
+        BookList.addBook(bookSample2);
+        CheckOutBooks.showBookListSelection();
+        assertThat(byteArrayOutputStream.toString(), containsString("Press 1 to checkout book below"));
+        assertThat(byteArrayOutputStream.toString(), containsString("Press 2 to checkout book below"));
+        assertThat(byteArrayOutputStream.toString(), not(containsString("Press 3 to checkout book below")));
     }
 }
