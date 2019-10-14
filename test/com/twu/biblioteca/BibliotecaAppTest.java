11f209f1d782598ce860cba163564cf25f4c0c68
Jie Yi Phua <jieyiphua@Jies-MacBook-Pro.local>
2019-10-14 16:40:29 +1100

    Refactored BibliotecaApp Test

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -7,13 +8,21 @@ import java.io.PrintStream;
 import static org.junit.Assert.*;
 
 public class BibliotecaAppTest {
-   @Test
-   public void printWelcomeMessage() {
-       final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-       System.setOut(new PrintStream(outContent));
-       BibliotecaApp app = new BibliotecaApp();
-       app.welcomeMessage();
-       assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n",outContent.toString());
-   }
+
+    private static ByteArrayOutputStream byteArrayOutputStream;
+    private static BibliotecaApp app;
+
+    @Before
+    public void setUp() {
+        app = new BibliotecaApp();
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+    }
+
+    @Test
+    public void testWelcomeMessage() {
+        app.welcomeMessage();
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n", byteArrayOutputStream.toString());
+    }
 
 }
