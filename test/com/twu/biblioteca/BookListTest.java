3ef8469f2b95ca89b2a0f66711aaa658f27073a6
Jie Yi Phua <jieyiphua@Jies-MacBook-Pro.local>
2019-10-14 16:42:17 +1100

    Refactored BookList Test

--- test/com/twu/biblioteca/BookListTest.java
+++ test/com/twu/biblioteca/BookListTest.java
@@ -1,6 +1,7 @@
 package com.twu.biblioteca;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -8,27 +9,47 @@ import java.io.PrintStream;
 import static org.junit.Assert.*;
 
 public class BookListTest {
+
+    private static ByteArrayOutputStream byteArrayOutputStream;
+    private static Book bookSample;
+    private static Book bookSample2;
+
+    @Before
+    public void setUp() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        bookSample = new Book("BookName","Author",1000);
+        bookSample2 = new Book("BookName2","Author2",2000);
+    }
+
     @After
     public void clearBookList() {
         BookList.bookList.clear();
     }
 
     @Test
-    public void canAddBook() {
-        Book expectedBook = new Book("BookName","Author",1000);
-        Book bookGotten = BookList.addBook(expectedBook).get(0);
-        assertEquals(bookGotten,expectedBook);
+    public void testAddBook() {
+        Book bookGotten = BookList.addBook(bookSample).get(0);
+        assertEquals(bookSample,bookGotten);
     }
 
     @Test
-    public void canGetBookList() {
-        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        Book book1 = new Book("BookName1","Author1",1000);
-        Book book2 = new Book("BookName2","Author2",2000);
-        BookList.addBook(book1);
-        BookList.addBook(book2);
+    public void testGetBookList() {
+        BookList.addBook(bookSample);
+        BookList.addBook(bookSample2);
         BookList.getBookList();
-        assertEquals("Book Name: " + book1.bookName + "\nAuthor: " + book1.author +"\nYear Published: " + book1.yearPublished + "\n\n" + "Book Name: " + book2.bookName + "\nAuthor: " + book2.author +"\nYear Published: " + book2.yearPublished + "\n\n",outContent.toString());
+        assertEquals(
+            "Book Name: " +
+            bookSample.bookName +
+            "\nAuthor: " +
+            bookSample.author +
+            "\nYear Published: " +
+            bookSample.yearPublished + "\n\n" +
+            "Book Name: " +
+            bookSample2.bookName +
+            "\nAuthor: " +
+            bookSample2.author +
+            "\nYear Published: " +
+            bookSample2.yearPublished + "\n\n", byteArrayOutputStream.toString());
     }
 }
