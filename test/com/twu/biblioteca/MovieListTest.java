03c0cf9db24b22decdd99fd3ad557ea2ac001291
Jie Yi Phua <jieyiphua@Jies-MacBook-Pro.local>
2019-10-15 10:45:30 +1100

    Refactored Movie List test

--- test/com/twu/biblioteca/MovieListTest.java
+++ test/com/twu/biblioteca/MovieListTest.java
@@ -1,6 +1,7 @@
 package com.twu.biblioteca;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -9,6 +10,18 @@ import static org.junit.Assert.*;
 
 public class MovieListTest {
 
+    private static ByteArrayOutputStream byteArrayOutputStream;
+    private static Movie movieSample;
+    private static Movie movieSample2;
+
+    @Before
+    public void setUp() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        movieSample = new Movie("MovieName","Director",1000,1);
+        movieSample2 = new Movie("MovieName2","Director2",2000,2);
+    }
+
     @After
     public void clearMovieList() {
         MovieList.movieList.clear();
@@ -16,20 +29,32 @@ public class MovieListTest {
 
     @Test
     public void canAddMovie() {
-        Movie movieGotten = new Movie("name","director",1000,2);
-        Movie expectedMovie = MovieList.addMovie(movieGotten).get(0);
-        assertEquals(movieGotten,expectedMovie);
+        Movie expectedMovie = MovieList.addMovie(movieSample).get(0);
+        assertEquals(movieSample, expectedMovie);
     }
 
     @Test
     public void canGetBookList() {
-        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        Movie movie1 = new Movie("name1","director1", 1000,1);
-        Movie movie2 = new Movie("name2", "director2", 2000,2);
-        MovieList.addMovie(movie1);
-        MovieList.addMovie(movie2);
+        MovieList.addMovie(movieSample);
+        MovieList.addMovie(movieSample2);
         MovieList.getMovieList();
-        assertEquals("Movie Name: " + "name1" + "\nDirector: " + "director1" + "\nYear Released: " + 1000 + "\nMovie Rating: " + 1 + "\n\nMovie Name: " + "name2" + "\nDirector: " + "director2" + "\nYear Released: " + 2000 + "\nMovie Rating: " + 2 + "\n\n",outContent.toString());
+        assertEquals(
+                "Movie Name: " +
+                movieSample.movieName +
+                "\nDirector: " +
+                movieSample.director +
+                "\nYear Released: " +
+                movieSample.yearReleased +
+                "\nMovie Rating: " +
+                movieSample.movieRating +
+                "\n\nMovie Name: " +
+                movieSample2.movieName +
+                "\nDirector: " +
+                movieSample2.director +
+                "\nYear Released: " +
+                movieSample2.yearReleased +
+                "\nMovie Rating: " +
+                movieSample2.movieRating +
+                "\n\n", byteArrayOutputStream.toString());
     }
 }
